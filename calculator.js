2e581d8bb077fc01e7f9759ae7272c6e102e11e5
Hakem Allouzi <hakemallouzi@Hakems-MacBook-Pro.local>
2021-05-19 00:40:26 +0300

    added this

--- calculator.js
+++ calculator.js
@@ -4,3 +4,24 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 // Run the code to see how to use prompt.
 const name = prompt("What's your name? ");
 console.log(`Hi, I'm ${name}.`);
+const num1 = parseInt(prompt("enter the first number: "));
+const num2 = parseInt(prompt("enter the seconde number: "));
+const operation = prompt("Please choose the operation (+, -, /, *): ")
+if(operation === '+'){
+ console.log(num1 + num2)
+}
+else if(operation === '-'){
+     console.log(num1 - num2)
+}
+else if(operation === '-'){
+     console.log(num1 * num2)
+}
+else if(operation === '-'){
+     console.log(num1 / num2)
+}
+else {
+console.log("innvalid")
+}
+
+
+
